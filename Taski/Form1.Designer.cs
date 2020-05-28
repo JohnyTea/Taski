1a9ab8b6e4a9ddbed58a01edee0b15beebc1b038
Łukasz Cydejko <johnatanherbatka@gmail.com>
2020-05-28 15:30:45 +0200

    Dodaj pliki projektów.

---
+++ Taski/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Taski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wartość dolna";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wartość górna";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Szukaj Pierwszych";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(154, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 399);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Wynik";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ile Tasków";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Taski liczby pierwsze";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Taski/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taski
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            int dolnaGranica = int.Parse(textBox1.Text);
+            int gornaGranica = int.Parse(textBox2.Text);
+            int ilosc = int.Parse(textBox4.Text);
+            if (dolnaGranica <= 0)
+            {
+                dolnaGranica = 1;
+            }
+            if (dolnaGranica > gornaGranica)
+            {
+                return;
+            }
+
+
+            //textBox3.Text = await Task.Run(() => zliczPierwsze(dolnaGranica, gornaGranica));
+
+
+            Task<int>[] taskArray = new Task<int>[ilosc];
+            int[] results = new int[ilosc];
+            int skok = (gornaGranica - dolnaGranica) / ilosc;
+            int dolna = dolnaGranica, gorna = dolnaGranica + skok;
+            for (int i = 0; i < taskArray.Length-1; i++)
+            {
+                int lewo = dolna;
+                int prawo = gorna;
+                taskArray[i] = new Task<int>(() => zliczPierwsze(lewo, prawo));
+                dolna = gorna+1;
+                gorna = dolna + skok;
+            }
+            int lewo2 = dolna;
+            int prawo2 = gornaGranica;
+            taskArray[ilosc-1] = new Task<int>(() => zliczPierwsze(lewo2, prawo2));
+            int sum = 0;
+
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                richTextBox1.Text += "Wywołano .Start dla Task pod indeskem: " + i + "\n";
+                taskArray[i].Start();
+            }
+
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                sum += await taskArray[i];
+                richTextBox1.Text += $"Task z indeksu {i} zwrocil wynik\n";
+            }
+
+            textBox3.Text = sum.ToString();
+
+        }
+        
+        private int zliczPierwsze(int dolnaGranica, int gornaGranica)
+        {
+            {
+                int iloscPierwszych = 0;
+                for (; dolnaGranica <= gornaGranica; dolnaGranica++)
+                {
+                    if (CzyPierwsza(dolnaGranica))
+                    {
+                        iloscPierwszych++;
+                    }
+                }
+                return iloscPierwszych;
+            }
+        }
+
+        private bool CzyPierwsza(int liczba)
+        {
+            for (int i = 2; i < liczba; i++)
+            {
+                if (liczba % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
